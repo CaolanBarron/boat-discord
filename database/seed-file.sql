32352778cf155c63ffe9e3850a76f07556fd2907
CaolanBarron <caolanbarron@protonmail.com>
2024-04-16 08:25:02 +0100

    Update seed-file.sql

--- database/seed-file.sql
+++ database/seed-file.sql
@@ -7,7 +7,17 @@ VALUES
     ('**"{0}"** {1} says as they almost slip on the deck.'),
     ('**"{0}"** {1} comments, whilst peeling a suspiciously fresh orange.'),
     ('**"{0}"** {1} emits, fumbling for their lucky coin as it rolls along the deck.'),
-    ('**"{0}"** {1} calls out from atop the boat, where they were attempting to adjust the receiver.');
+    ('**"{0}"** {1} calls out from atop the boat, where they were attempting to adjust the receiver.'),
+('**"{0}"** {1} grunts.'),
+('**"{0}"** {1} states dismissively.'),
+('**"{0}"** {1} slurrs in between swigs of ale.'),
+('**"{0}"** {1} says.'),
+('**"{0}"** {1} shouts from somewhere deep in the boat.'),
+('**"{0}"** {1} whispers, afraid of whats listening.'),
+('**"{0}"** {1} answers cryptically.'),
+('**"{0}"** {1} exclaims, jumping to their feet!'),
+('**"{0}"** {1} comments, thinking about the land.'),
+('**"{0}"** {1} announces, to nobodies ears.'),;
 
 DELETE FROM skills;
 
