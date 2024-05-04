9dccc651068f7b4c53b66ac49f590673b7f61df0
Caolan Barron <A00274885@student.ait.ie>
2024-05-04 17:12:50 +0100

    Implemented more display map dev tool functionality

--- src/commands/utility/dev.js
+++ src/commands/utility/dev.js
@@ -1,4 +1,9 @@
-import { SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  SlashCommandBuilder,
+} from "discord.js";
 
 export default {
   data: new SlashCommandBuilder()
@@ -11,17 +16,106 @@ export default {
         .setRequired(true)
         .addChoices(
           { name: "map", value: "tool_map" },
-          { name: "boat", value: "tool_boat" },
-        ),
+          { name: "boat", value: "tool_boat" }
+        )
     ),
 
   async execute(interaction) {
     console.log(interaction.options.getString("tools"));
+    const row = new ActionRowBuilder();
     switch (interaction.options.getString("tools")) {
       case "tool_map":
-        console.log("Mapping");
+        const displayMapButton = new ButtonBuilder()
+          .setCustomId("dev_display_map")
+          .setLabel("Display Map")
+          .setStyle(ButtonStyle.Primary);
+
+        row.addComponents(displayMapButton);
         break;
     }
-    await interaction.reply("lol");
+
+    const response = await interaction.reply({ components: [row] });
+
+    const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+    try {
+      const confirmation = await response.awaitMessageComponent({
+        filter: collectorFilter,
+        time: 60_000,
+      });
+      if (confirmation.customId === "dev_display_map") {
+        await confirmation.update({
+          content: `map:\n🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n
+🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦`,
+          components: [],
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      await interaction.editReply({
+        content: "Confirmation not recieved within 1 minute, cancelling",
+        components: [],
+      });
+    }
   },
 };
+
+// mapData function injected
+// Should return an object of data
+// {
+//    boats: [],
+//    biomes: []
+// }
+export function generateMap(mapData) {
+  const map = [];
+
+  for (let outer = 0; outer < 20; outer++) {
+    const row = [];
+    for (let inner = 0; inner < 20; inner++) {
+      row.push("🟦");
+    }
+    map.push(row);
+  }
+
+  const { boats, biomes } = mapData;
+
+  // step through boats and add each of them to the map
+
+  for (const boat of boats) {
+    map[boat.y_coord][boat.x_coord] = "⛵";
+  }
+
+  // step through biomes and add each of them to the map
+
+  // construct map into a single string
+  let finalMap = "";
+  for (const outterPiece of map) {
+    let row = "";
+    for (const innerPiece of outterPiece) {
+      row = row.concat(innerPiece);
+    }
+    finalMap = finalMap.concat(row + "\n");
+  }
+  console.log(finalMap);
+  return finalMap;
+}
+
+function getMapData() {}
