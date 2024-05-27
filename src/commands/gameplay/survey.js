8dbc62f4ae9a3e028bfb2e9ef79db264d067179a
Caolan Barron <A00274885@student.ait.ie>
2024-05-27 01:19:31 +0100

    Added the survey command

---
+++ src/commands/gameplay/survey.js
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "discord.js";
+import db from "../../../database/database.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("survey")
+    .setDescription("Describes the environment around The Boat."),
+  async execute(interaction) {
+    try {
+      const boat = db()
+        .prepare(
+          `SELECT * 
+        FROM boat 
+        LEFT JOIN biome_coords 
+        ON boat.x_coord = biome_coords.x_coord AND boat.y_coord = biome_coords.y_coord 
+        LEFT JOIN biomes ON biome_coords.biome_key = biomes.key 
+        WHERE boat.id = ?`
+        )
+        .get(interaction.guildId);
+
+      let response = "The Boat is currently in a nondescript part of the ocean";
+
+      if (boat.biome_key)
+        response = `The Boat is currently in the ${boat.name} biome.`;
+      await interaction.reply({ content: response, ephemeral: true });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
