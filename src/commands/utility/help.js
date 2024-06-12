4292f6858934d0f5548aa46cab990f65a41ad5e0
Caolan Barron <A00274885@student.ait.ie>
2024-06-12 14:19:52 +0100

    Added options to the help command

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -11,6 +11,17 @@ export default {
                     'Information about a particular part of the game '
                 )
                 .addChoices({ name: 'Activities', value: 'topic_activities' })
+                .addChoices({ name: 'Events', value: 'topic_events' })
+                .addChoices({ name: 'Items', value: 'topic_items' })
+                .addChoices({ name: 'World', value: 'topic_world' })
+                .addChoices({ name: 'World', value: 'topic_world' })
+                .addChoices({ name: 'The Boat', value: 'topic_boat' })
+                .addChoices({ name: 'Fishing', value: 'topic_fishing' })
+                .addChoices({ name: 'Cartography', value: 'topic_cartography' })
+                .addChoices({ name: 'Repair', value: 'topic_repair' })
+                .addChoices({ name: 'Research', value: 'topic_research' })
+                .addChoices({ name: 'Sailing', value: 'topic_sailing' })
+                .addChoices({ name: 'Salvage', value: 'topic_salvage' })
         ),
     async execute(interaction) {
         await interaction.reply(`Helpful information lol`);
